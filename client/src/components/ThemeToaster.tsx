b6f7f0415c48b34ab21c413966586429aa36dd08
benjamaa00 <benjamaa000@gmail.com>
2026-07-21 02:09:42 +0100

    fix: add missing ThemeToaster component to git

---
+++ client/src/components/ThemeToaster.tsx
@@ -0,0 +1,9 @@
+import { memo } from "react";
+import { Toaster } from "sonner";
+import { useThemeContext } from "../contexts/ThemeContext";
+
+export const ThemeToaster = memo(() => {
+  const { theme } = useThemeContext();
+  return <Toaster position="top-center" richColors theme={theme} />;
+});
+ThemeToaster.displayName = "ThemeToaster";
